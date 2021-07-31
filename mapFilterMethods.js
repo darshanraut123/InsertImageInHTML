16a8edf9c4d12eb71cb6562d2cf1ead84f48de1c
Darshan Raut <56134448+darshanraut123@users.noreply.github.com>
2021-07-31 15:12:24 +0530

    Create mapFilterMethods.js
    Excercise for date 31/July/2021 Saturday 
    all female names
    and
    starting with A names
    count of no of males

---
+++ mapFilterMethods.js
@@ -0,0 +1,8 @@
+
+let names = [{name: "Arti", gender: "female"},{name: "Shubham", gender: "male"},{name: "Akshaya", gender: "female"},{name: "Aman", gender: "male"},{name: "Pooja", gender: "female"},{name: "Akshay", gender: "male"}];
+//All Female names:
+names.filter(n=>n.name.toLowerCase().startsWith("a").forEach((val => console.log(val.name)))
+//All names starting with A : 
+names.filter(n=>n.name.toLowerCase().startsWith("a")).forEach((val)=>console.log(val.name));
+//No of Males:
+console.log((names.filter(n=>n.gender=="male")).length);
