57f90d151f841024a188c4983eb096ebfeba2526
Darshan Raut <56134448+darshanraut123@users.noreply.github.com>
2021-07-31 15:20:53 +0530

    Update mapFilterMethods.js
    Added missing ;

--- mapFilterMethods.js
+++ mapFilterMethods.js
@@ -1,7 +1,7 @@
 
 let names = [{name: "Arti", gender: "female"},{name: "Shubham", gender: "male"},{name: "Akshaya", gender: "female"},{name: "Aman", gender: "male"},{name: "Pooja", gender: "female"},{name: "Akshay", gender: "male"}];
 //All Female names:
-names.filter(n=>n.name.toLowerCase().startsWith("a")).forEach((val => console.log(val.name)))
+names.filter(n=>n.name.toLowerCase().startsWith("a")).forEach((val => console.log(val.name)));
 //All names starting with A : 
 names.filter(n=>n.name.toLowerCase().startsWith("a")).forEach((val)=>console.log(val.name));
 //No of Males:
